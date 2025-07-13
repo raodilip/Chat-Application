17873ca51078aa87bc3ff80fee10a1086d6df7ac
Dilip Rao <diliprao@Dilips-M2-MacBook-Pro.local>
2025-07-13 21:24:30 +0530

    One more user added.

--- src/data/mockdata.js
+++ src/data/mockdata.js
@@ -3,11 +3,14 @@ export const friends = [
     { id: 2, name: "Bob" },
     { id: 3, name: "Charlie" },
     { id: 4, name: "Dilip" },
+    { id: 5, name: "Vinutha" },
   ];
   
   export const chats = {
     1: [{ sender: "You", message: "Hi Alice!" }],
     2: [{ sender: "You", message: "Hey Bob!" }],
-    3: [],
+    3: [{}],
+    4: [{}],
+    5: [{}]
   };
   
